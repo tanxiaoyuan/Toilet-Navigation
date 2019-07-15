903de082de620d217ac8a84fdc42219909f243c1
tanxiaoyuan <925707761@qq.com>
2019-07-15 23:24:12 +0800

    加入地图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,65 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const qqmapsdk = app.globalData.qqmapsdk
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+     
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+    var that = this;
+     wx.getLocation({
+       type:"wgs84",
+       success: function(res) {
+         that.setData({
+            'longitude':res.longitude,
+            'latitude':res.latitude,
+            'iconPath':"../../images/self.png",
+            'id':res.id
+          })
+       },
+     })
+     
+  },
+  onShow: function () {
+    var that = this;
+    // 调用接口
+    qqmapsdk.search({
+      keyword: 'WC',  //搜索关键词
+      success: function (res) { //搜索成功后的回调
+        var mks = []
+        for (var i = 0; i < res.data.length; i++) {
+          mks.push({ // 获取返回结果，放到mks数组中
+            title: res.data[i].title,
+            id: res.data[i].id,
+            latitude: res.data[i].location.lat,
+            longitude: res.data[i].location.lng,
+            iconPath: "../../images/toilet.png", //图标路径
+            width: 20,
+            height: 20
           })
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+        mks.push({ // 获取返回结果，放到mks数组中
+           
+          id: that.data.id,
+          latitude: that.data.latitude,
+          longitude: that.data.longitude,
+          iconPath: that.data.iconPath, //图标路径
+          width: 20,
+          height: 20
+        })
+        that.setData({ //设置markers属性，将搜索结果显示在地图中
+          markers: mks
+        })
+      },
+      fail: function (res) {
+        console.log(res);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
+    });
   }
+  
 })
